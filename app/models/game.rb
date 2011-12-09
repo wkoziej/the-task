95a1cffe7bfc0dac2173a0c1fc8caa342917ba4a
wkoziej <wkoziej@ffc9e89f-0e74-392b-65b1-b743d7d3f50e>
2011-12-09 07:38:49 +0000

    Initial test of state_machine

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,13 +1,35 @@
 class Game < ActiveRecord::Base
+  # Relations
   belongs_to :creator, :class_name => "User", :foreign_key => :creator_id
   has_many :challenges
   has_many :plays
 
+  # Validations
   validate :status, :presence => true
+  validate :creator, :presence => true
 
   def initialize(params = nil) 
-    super(nil) 
-    self.status = "NEW"
+    super() 
+    #self.status = "NEW"
   end 
+  
+  state_machine :status, :initial => :new do
+    
+    state :active do
+    end
+
+    state :archived do
+    end
+  
+    event :activate do
+      transition :new => :active
+    end
+    
+    event :archive do
+      transition :active => :archived
+    end
+    
+  end
+
 
 end
