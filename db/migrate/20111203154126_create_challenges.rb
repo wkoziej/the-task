c9a253955fef21118527da1677288079e1060950
wkoziej@gmail.com <wkoziej@gmail.com@ffc9e89f-0e74-392b-65b1-b743d7d3f50e>
2011-12-13 17:33:52 +0000

    Model extension

--- db/migrate/20111203154126_create_challenges.rb
+++ db/migrate/20111203154126_create_challenges.rb
@@ -2,10 +2,14 @@ class CreateChallenges < ActiveRecord::Migration
   def change
     create_table :challenges do |t|
       t.integer :points
+      t.references :pointKind
       t.string :title
       t.text :description
       t.references :game
       t.string :status
+      t.string :type
+      t.float :longitude 
+      t.float :latitude
 
       t.timestamps
     end
--- db/migrate/20111203215108_populate_challenges.rb
+++ db/migrate/20111203215108_populate_challenges.rb
@@ -5,15 +5,18 @@ class PopulateChallenges < ActiveRecord::Migration
     challenge.points = 5
     challenge.title = "Capture Code in XCaffee"
     challenge.description = "Ask barman for code"
-    challenge.status = "ACTIVE"
-    challenge.save()
-
+    if not challenge.save()
+      say "Errors during save" 
+      challenge.errors.each do |attribute, errors_array|
+        say attribute
+        say errors_array
+      end
+    end
     challenge = Challenge.new
     challenge.game = Game.find(1)
     challenge.points = 10
     challenge.title = "Order beer in 17 Pub :)"
     challenge.description = "Order one beer and ask for code"
-    challenge.status = "ACTIVE"
     challenge.save()
 
   end
