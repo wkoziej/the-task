1fb1d44d33a38e4745e4ba05cd4d6c50ffb24b69
wkoziej@gmail.com <wkoziej@gmail.com@ffc9e89f-0e74-392b-65b1-b743d7d3f50e>
2011-12-30 22:54:03 +0000

    Layouts

--- trunk/db/migrate/20111203151018_create_games.rb
+++ trunk/db/migrate/20111203151018_create_games.rb
@@ -5,7 +5,11 @@ class CreateGames < ActiveRecord::Migration
       t.text :description, :null => false
       t.string :status, :null => false
       t.references :creator, :null => false
-
+      t.string :image_file_name
+      t.string :image_content_type
+      t.integer :imgae_file_size
+      t.datetime :image_updated_at
+      
       t.timestamps
     end
     add_index :games, :creator_id
