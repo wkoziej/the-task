cbc00748b8e8a18fb57eb8f82289e2880f3278d6
wkoziej@gmail.com <wkoziej@gmail.com@ffc9e89f-0e74-392b-65b1-b743d7d3f50e>
2012-01-05 19:51:00 +0000

    resolved #11 issue. Creating wright player_challenges

--- trunk/test/unit/play_test.rb
+++ trunk/test/unit/play_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class PlayTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  
+  test "creation of game with enterMessage and captureCode challenges" do
+    @game = games(:one)
+    @player = users(:one)
+    @params = { :game_id => @game.id, :player_id => @player.id };
+    @play = Play.new(@params)
+    assert_equal @game.id, @play.game.id 
+    assert_equal @player.id, @play.player.id
+    assert_equal 2, @play.player_challenges.count, "Too litle challenges"
+    assert_equal "CapturingCode", @play.player_challenges[0].class.to_s
+    assert_equal "EnteringMessage", @play.player_challenges[1].class.to_s
+
+  end
 end
